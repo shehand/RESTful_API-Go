e1b14d7b32ff8b39dc3f75851a928cc033eefebf
shehand <sonodewcomplex@gmail.com>
2019-05-14 09:43:15 +0530

    add create account api call

---
+++ controllers/authController.go
@@ -0,0 +1,20 @@
+package controllers
+
+import (
+	"net/http"
+	u "../utils"
+	"../models"
+	"encoding/json"
+)
+
+var CreateAccount = func(w http.ResponseWriter, r *http.Request) {
+	account := &models.Account{}
+	err := json.NewDecoder(r.Body).Decode(account)
+	if err != nil {
+		u.Respond(w, u.Message(false, "Invalid request"))
+		return
+	}
+
+	resp := account.Create() //Create account
+	u.Respond(w, resp)
+}
