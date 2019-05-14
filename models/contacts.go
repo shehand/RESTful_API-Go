780c68e97ce7bf78b6ed40452c2b7be6f9ef50ba
shehand <sonodewcomplex@gmail.com>
2019-05-14 09:37:45 +0530

    add validate contact

--- models/contacts.go
+++ models/contacts.go
@@ -1,1 +1,32 @@
 package models
+
+import (
+	u "../utils"
+	"github.com/jinzhu/gorm"
+	"fmt"
+)
+
+type Contact struct {
+	gorm.Model
+	Name string `json:"name"`
+	Phone string `json:"phone"`
+	UserId uint `json:"user_id"` //The user that this contact belongs to
+}
+
+func (contact *Contact) Validate() (map[string] interface{}, bool) {
+
+	if contact.Name == "" {
+		return u.Message(false, "Contact name should be on the payload"), false
+	}
+
+	if contact.Phone == "" {
+		return u.Message(false, "Phone number should be on the payload"), false
+	}
+
+	if contact.UserId <= 0 {
+		return u.Message(false, "User is not recognized"), false
+	}
+
+	//All the required parameters are present
+	return u.Message(true, "success"), true
+}
