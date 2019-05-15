3aaed964813c64ebcf1a06983c9534cd49035777
shehand <sonodewcomplex@gmail.com>
2019-05-15 10:08:07 +0530

    add address controller with get address api method

--- controllers/addressController.go
+++ controllers/addressController.go
@@ -1,1 +1,26 @@
 package controllers
+import (
+	"../models"
+	u "../utils"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+)
+
+func CreateAddresses(){}
+
+var GetAddresses = func (w http.ResponseWriter, r *http.Request){
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		//The passed path parameter is not an integer
+		u.Respond(w, u.Message(false, "There was an error in your request"))
+		return
+	}
+
+	data := models.GetAddresses(uint(id))
+	resp := u.Message(true, "success")
+	resp["data"] = data
+	u.Respond(w, resp)
+}
