c52a99bfc493ba85999d105b8f3e231294e6fe59
shehand <sonodewcomplex@gmail.com>
2019-05-16 11:02:51 +0530

    add add contacts api call

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main(){
 	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
 	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
 	router.HandleFunc("/api/me/contacts", controllers.GetContactsFor).Methods("GET")
+	router.HandleFunc("/api/me/contacts", controllers.CreateContact).Methods("POST")
 	router.HandleFunc("api/me/addresses", controllers.GetAddresses).Methods("GET")
 	router.HandleFunc("api/me/addresses", controllers.CreateAddresses).Methods("POST")
 
