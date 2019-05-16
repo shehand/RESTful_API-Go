e8e222844e46f519cddb711503e1d5952e73dca0
shehand <sonodewcomplex@gmail.com>
2019-05-16 11:06:51 +0530

    add user struct

--- models/users.go
+++ models/users.go
@@ -1,1 +1,15 @@
 package models
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	u "../utils"
+
+)
+
+type User struct {
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+	DOB string `json:"dob"`
+	Email string `json:"email"`
+} 
