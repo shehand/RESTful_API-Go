4adcaa67275545fded6917e1c989847b15183cb1
shehand <sonodewcomplex@gmail.com>
2019-05-16 11:13:45 +0530

    Add migrations for users and addresses

--- models/base.go
+++ models/base.go
@@ -31,7 +31,7 @@ func init(){
 	}
 
 	db = conn
-	db.Debug().AutoMigrate(&Account{}, &Contact{}) //Database migration
+	db.Debug().AutoMigrate(&Account{}, &Contact{}, &User{}, &Address{}) //Database migration
 
 }
 
